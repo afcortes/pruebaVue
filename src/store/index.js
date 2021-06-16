2b60fb9ee6b1ca7100beceee86941cfb8f6f00e8
afcortes <af.cortes26999@gmail.com>
2021-06-16 09:51:21 -0500

    primer commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    data: null
   },
   mutations: {
+    getCharacters(state, payload) {
+      state.data = payload
+    }
   },
   actions: {
+    async getCharactersAction({ commit }) {
+      const response = await axios.get('https://rickandmortyapi.com/api/character')
+      commit('getCharacters', response.data.results)
+    }
   },
   modules: {
+  },
+  getters: {
+    getData: state => state.data
   }
 })
